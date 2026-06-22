362d645373dcb2dcdaab0ce35708aae5b1b6e86e
Adrian Berger <adrian.berger@natron.io>
2026-06-22 10:32:28 +0200

    chore: release v1.3.1

--- version/version.go
+++ version/version.go
@@ -6,8 +6,8 @@ package version
 import "github.com/hashicorp/packer-plugin-sdk/version"
 
 var (
-	Version           = "1.3.0"
-	VersionPrerelease = "rc.5"
+	Version           = "1.3.1"
+	VersionPrerelease = ""
 	VersionMetadata   = ""
 	PluginVersion     = version.NewPluginVersion(Version, VersionPrerelease, VersionMetadata)
 )
